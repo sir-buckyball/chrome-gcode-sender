a57586e89b8e6afa713149177f68adb0a1730a90
Buck Clay <clayb.rpi@gmail.com>
2014-07-26 10:08:09 -0700

    Fix default settings
    Improve the new-user experience by properly reading default settings.

--- src/settings.js
+++ src/settings.js
@@ -43,19 +43,45 @@ app.service('settingsService', function($rootScope) {
         s = o.settings;
       }
 
-      settings.workspace_width_mm = Number(s["workspace-width-mm"]) || 150;
-      settings.workspace_depth_mm = Number(s["workspace-depth-mm"]) || 150;
-      settings.workspace_height_mm = Number(s["workspace-height-mm"]) || 50;
-      settings.workspace_port = s["workspace-port"] || "";
-      settings.workspace_baud = Number(s["workspace-baud"]) || 115200;
-      settings.workspace_show_estop = s["workspace-show-estop"] || false;
-      settings.workspace_show_home = s["workspace-show-home"] || false;
-      settings.workspace_show_spindle = s["workspace-show-spindle"] || false;
-      settings.workspace_show_zero = s["workspace-show-zero"] || false;
-      settings.workspace_jog_feedrate = Number(s["workspace-jog-feedrate"]) || 0;
-      settings.workspace_jog_rapid = s["workspace-jog-rapid"] || false;
-      settings.gcode_preamble = s["gcode-preamble"] || "";
-      settings.gcode_postamble = s["gcode-postamble"] || "";
+      if (s["workspace-width-mm"] !== undefined) {
+        settings.workspace_width_mm = Number(s["workspace-width-mm"]);
+      }
+      if (s["workspace-depth-mm"] !== undefined) {
+        settings.workspace_depth_mm = Number(s["workspace-depth-mm"]);
+      }
+      if (s["workspace-height-mm"] !== undefined) {
+        settings.workspace_height_mm = Number(s["workspace-height-mm"]);
+      }
+      if (s["workspace-port"] !== undefined) {
+        settings.workspace_port = s["workspace-port"];
+      }
+      if (s["workspace-baud"] !== undefined) {
+        settings.workspace_baud = Number(s["workspace-baud"]);
+      }
+      if (s["workspace-show-estop"] !== undefined) {
+        settings.workspace_show_estop = s["workspace-show-estop"];
+      }
+      if (s["workspace-show-home"] !== undefined) {
+        settings.workspace_show_home = s["workspace-show-home"];
+      }
+      if (s["workspace-show-spindle"] !== undefined) {
+        settings.workspace_show_spindle = s["workspace-show-spindle"];
+      }
+      if (s["workspace-show-zero"] !== undefined) {
+        settings.workspace_show_zero = s["workspace-show-zero"];
+      }
+      if (s["workspace-jog-feedrate"] !== undefined) {
+        settings.workspace_jog_feedrate = Number(s["workspace-jog-feedrate"]);
+      }
+      if (s["workspace-jog-rapid"] !== undefined) {
+        settings.workspace_jog_rapid = s["workspace-jog-rapid"];
+      }
+      if (s["gcode-preamble"] !== undefined) {
+        settings.gcode_preamble = s["gcode-preamble"];
+      }
+      if (s["gcode-postamble"] !== undefined) {
+        settings.gcode_postamble = s["gcode-postamble"];
+      }
       console.log("settings loaded from storage.\n" + JSON.stringify(settings));
       $rootScope.$apply();
     });
